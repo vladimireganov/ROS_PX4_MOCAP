9ffc85f90a820a6394b5bfd5ff1c1830c7d38df9
VovaE <volodya.eganov.v@ya.ru>
2022-07-13 12:38:29 -0500

    update on plotter

--- analysis/logger_plot.py
+++ analysis/logger_plot.py
@@ -5,9 +5,9 @@ import pandas as pd
 def read_data(file):
     return pd.read_csv(file,sep=",")
 
-postion = read_data()
-state = read_data()
-set_point = read_data()
+postion = read_data("analysis/position.csv")
+state = read_data("analysis/state.csv")
+set_point = read_data("analysis/target_position.csv")
 
 fig = plt.figure()
 ax = plt.subplot()
@@ -38,3 +38,11 @@ ax.set_ylabel("Position Z")
 l1, = ax.plot(postion["stamp"],postion["position_z"],color='orange',label="position_z")
 ax.legend(handles=[l1])
 plt.grid()
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.set_title("3D position") # again passes data from all are being overwritten by passed data
+ax.scatter3D(postion["position_x"], postion["position_y"], postion["position_z"], 'gray')
+
+
+plt.show()
