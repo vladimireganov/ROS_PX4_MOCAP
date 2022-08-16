939949a0bf6eef3549fa6cb09da9849a3108bec0
VovaE <volodya.eganov.v@ya.ru>
2022-08-16 16:44:05 -0500

    working bash scripts

--- extract_data.sh
+++ extract_data.sh
@@ -3,8 +3,12 @@
 FILES="/Volumes/GAMEZ/px4_data/faster_logging/log/2022-06-20/*"
 for f in $FILES
 do
-  mkdir "${file%.*}"
-  ulog2csv $f -o "${file%.*}/"
+  echo "$f"
+  base=${f%.ulg}
+#   mkdir "$base"
+  echo "$base/"
+  ulog2csv -o $base/ $f 
+#   echo "$base/"
   # take action on each file. $f store current file name
 #   cat "$f"
 done
