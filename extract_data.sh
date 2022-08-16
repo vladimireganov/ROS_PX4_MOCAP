bad692762ebd2931c607f1dce0a47da10a3b2ab7
Vladimir Eganov <38359772+vladimireganov@users.noreply.github.com>
2022-08-16 14:30:40 -0500

    exctract data

--- extract_data.sh
+++ extract_data.sh
@@ -3,7 +3,8 @@
 FILES="/Volumes/GAMEZ/px4_data/faster_logging/log/2022-06-20/*"
 for f in $FILES
 do
-  ulog2csv $f
+  mkdir "${file%.*}"
+  ulog2csv $f -o "${file%.*}/"
   # take action on each file. $f store current file name
 #   cat "$f"
 done
